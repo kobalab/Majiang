e4125bf39151b0576e1902176464bebd585d3350
Satoshi Kobayashi <koba@yk.rim.or.jp>
2017-01-03 00:47:26 +0900

    #5 牌譜のタイトル編集のUIを変更

--- js/editor.js
+++ js/editor.js
@@ -8,8 +8,32 @@ Majiang.View.PaipuEditor = function(node, paipu) {
 Majiang.View.PaipuEditor.prototype.redraw = function() {
     
     var self = this;
-    this._node.find('.title textarea').text(this._paipu.title);
+    
+    this.update_title();
+    this.set_title_handler();
+}
+
+Majiang.View.PaipuEditor.prototype.update_title = function() {
+
+    var title_html = this._paipu.title
+                        .split(/\n/)
+                        .map(function(str){return $('<p>').text(str).html()})
+                        .join('<br>');
+    this._node.find('.title div').html(title_html).show();
+    this._node.find('.title textarea').val(this._paipu.title).hide();
+}
+
+Majiang.View.PaipuEditor.prototype.set_title_handler = function() {
+
+    var self = this;
+
+    this._node.find('.title').off('click').on('click', function(){
+        $(this).find('div').hide();
+        $(this).find('textarea').show();
+    });
+    
     this._node.find('.title textarea').off('focusout').on('focusout', function(){
-        self._paipu.title = $(this).val().replace(/\n+$/,'');
+        self._paipu.title = $(this).val().replace(/\n+$/,'') || '(牌譜名)';
+        self.update_title();
     });
 }
