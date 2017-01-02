fe141f083b87bafa0c5cd61708519b011265ea93
Satoshi Kobayashi <koba@yk.rim.or.jp>
2017-01-03 08:49:36 +0900

    #5 局数を編集できるようにした

--- js/editor.js
+++ js/editor.js
@@ -3,19 +3,33 @@
 Majiang.View.PaipuEditor = function(node, paipu) {
     this._node  = node;
     this._paipu = paipu;
+    this._log_idx = 0;
+}
+
+Majiang.View.PaipuEditor.prototype.player_id = function(l) {
+    return (this._paipu.qijia
+            + this._paipu.log[this._log_idx][0].qipai.jushu
+            + l) % 4;
 }
 
 Majiang.View.PaipuEditor.prototype.redraw = function() {
     
     var self = this;
     
+    this._node.hide();
+    
     this.update_title();
     this.set_title_handler();
     
+    this.update_jushu();
+    this.set_jushu_handler();
+
     for (var l = 0; l < 4; l++) {
         this.update_player(l);
         this.set_player_handler(l);
     }
+
+    this._node.fadeIn();
 }
 
 Majiang.View.PaipuEditor.prototype.update_title = function() {
@@ -43,10 +57,61 @@ Majiang.View.PaipuEditor.prototype.set_title_handler = function() {
     });
 }
 
+Majiang.View.PaipuEditor.prototype.update_jushu = function() {
+
+    var feng_hanzi = ['東','南','西','北'];
+    var shu_hanzi  = ['一','二','三','四'];
+
+    var qipai = this._paipu.log[this._log_idx][0].qipai;
+    
+    this._node.find('.jushu div').text(
+                          feng_hanzi[qipai.zhuangfeng]
+                        + shu_hanzi[qipai.jushu]
+                        + '局').show();
+    var select = this._node.find('.jushu select').empty().hide();
+    for (var zhuangfeng = 0; zhuangfeng < 4; zhuangfeng++) {
+        for (var jushu = 0; jushu < 4; jushu++) {
+            var option = $('<option>').text(
+                          feng_hanzi[zhuangfeng]
+                        + shu_hanzi[jushu]
+                        + '局')
+                        .val(zhuangfeng * 4 + jushu);
+            if (zhuangfeng == qipai.zhuangfeng && jushu == qipai.jushu) {
+                option.attr('selected', 'selected')
+            }
+            select.append(option);
+        }
+    }
+}
+
+Majiang.View.PaipuEditor.prototype.set_jushu_handler = function() {
+
+    var self = this;
+
+    this._node.find('.jushu').off('click').on('click', function(){
+        $(this).find('div').hide();
+        $(this).find('select').show();
+    });
+
+    this._node.find('.jushu').off('focusout').on('focusout', function(){
+        $(this).find('select').hide();
+        $(this).find('div').show();
+    });
+    this._node.find('.jushu select').off('change').on('change', function(){
+        self._paipu.log[self._log_idx][0].qipai.zhuangfeng
+                                    = Math.floor($(this).val() / 4);
+        self._paipu.log[self._log_idx][0].qipai.jushu
+                                    = Math.floor($(this).val() % 4);
+        self.redraw();
+    });
+}
+
 Majiang.View.PaipuEditor.prototype.update_player = function(l) {
 
-    this._node.find('.player .name').eq(l).text(this._paipu.player[l]).show();
-    this._node.find('.player input').eq(l).val(this._paipu.player[l]).hide();
+    var id = this.player_id(l);
+
+    this._node.find('.player .name').eq(l).text(this._paipu.player[id]).show();
+    this._node.find('.player input').eq(l).val(this._paipu.player[id]).hide();
 }
 
 Majiang.View.PaipuEditor.prototype.set_player_handler = function(l) {
@@ -60,7 +125,7 @@ Majiang.View.PaipuEditor.prototype.set_player_handler = function(l) {
 
     this._node.find('.player input').eq(l)
                                     .off('focusout').on('focusout', function(){
-        self._paipu.player[l] = $(this).val();
+        self._paipu.player[self.player_id(l)] = $(this).val();
         self.update_player(l);
     });
 }
