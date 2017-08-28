a55a4ed3ec574f4b593fce4d7c73c43c0d2f1765
Satoshi Kobayashi <koba@yk.rim.or.jp>
2017-08-28 22:12:21 +0800

    #24 webpackによるbuildのタスクを追加

---
+++ src/js/majiang.js
@@ -0,0 +1,9 @@
+/*
+ *  Majiang
+ *
+ *  Copyright(C) 2017 Satoshi Kobayashi
+ *  Released under the MIT license
+ *  https://github.com/kobalab/Majiang/blob/master/LICENSE
+ */
+
+Majiang = { VERSION: '1.0.0' };
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry:  './src/js/majiang.js',
+    output: {
+        path:     __dirname + '/www/js/',
+        filename: 'majiang.js'
+    },
+};
