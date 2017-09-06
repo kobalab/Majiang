1a40107ff4b49f942fef2f1344e923dd0e6fbf6e
Satoshi Kobayashi <koba@yk.rim.or.jp>
2017-09-07 05:32:00 +0800

    #24 インデントを修正

--- test/00.majiang.js
+++ test/00.majiang.js
@@ -4,16 +4,16 @@ const semver  = require('semver');
 const Majiang = require('../src/js/majiang');
 
 suite('Majiang', function(){
-    test('モジュールがが存在すること', function(){assert.ok(Majiang);});
-    suite('#VERSION', function(){
-        test('バージョン番号が存在すること', function(){
-            assert.ok(Majiang.VERSION);
-        });
-        test('バージョン番号の形式が正しいこと', function(){
-            assert.ok(semver.valid(Majiang.VERSION));
-        });
-        test('メジャー・バージョン番号が 1 であること', function(){
-            assert.ok(semver.satisfies(Majiang.VERSION, '<2 >=1'));
-        });
+  test('モジュールがが存在すること', function(){assert.ok(Majiang);});
+  suite('VERSION', function(){
+    test('バージョン番号が存在すること', function(){
+      assert.ok(Majiang.VERSION);
     });
+    test('バージョン番号の形式が正しいこと', function(){
+      assert.ok(semver.valid(Majiang.VERSION));
+    });
+    test('メジャー・バージョン番号が 1 であること', function(){
+      assert.ok(semver.satisfies(Majiang.VERSION, '<2 >=1'));
+    });
+  });
 });
