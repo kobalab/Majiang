764b253a1523eef55af1603c39c5e6d47f16bcca
Satoshi Kobayashi <koba@yk.rim.or.jp>
2017-09-24 17:05:36 +0800

    #24 Majiang.View をリファクタリング
    - Nodeのモジュールとした
    - 牌表示の機能を Majiang.View.pai に移動した。

---
+++ src/js/majiang/view.js
@@ -0,0 +1,11 @@
+/*
+ *  Majiang.View
+ */
+"use strict";
+
+module.exports = {
+    pai:     require('./view/pai'),
+    Shoupai: require('./view/shoupai'),
+    Shan:    require('./view/shan'),
+    He:      require('./view/he')
+};
