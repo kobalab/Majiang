b9c04a18807de915563be0e9b7452cc3744f4fe1
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-04-14 00:54:58 +0800

    #24 ファイルダウンロードできない端末、ドラッグ&ドロップができない端末に対応

--- src/js/majiang/view/paipufile.js
+++ src/js/majiang/view/paipufile.js
@@ -205,6 +205,15 @@ redraw() {
         }
     });
 
+    let ua = navigator.userAgent;
+    if (ua.match(/\bMobile\b/)) {
+        $('.download', this._node).hide();
+        $('.move',     this._node).hide();
+    }
+    if (ua.match(/\bMSIE\b/) || ua.match(/\bTrident\b/)) {
+        $('.download', this._node).hide();
+    }
+
     $('.row', this._node).fadeIn();
 
     return this;
