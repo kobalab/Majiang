baec0419a92158688e5e2086c209c57d9942464a
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-05-13 00:00:05 +0800

    v1.1.1 リリース

--- src/js/majiang.js
+++ src/js/majiang.js
@@ -10,7 +10,7 @@
 "use strict";
 
 module.exports = {
-    VERSION: '1.1.0',
+    VERSION: '1.1.1',
     Shoupai: require('./majiang/shoupai'),
     Shan:    require('./majiang/shan'),
     He:      require('./majiang/he'),
