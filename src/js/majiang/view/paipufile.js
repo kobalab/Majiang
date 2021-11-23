d421ce3c5f1d46234446140af9d5b80a2c22e3e7
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-11-23 22:20:52 +0800

    HTTP2 のサーバから牌譜を取得する際にエラーメッセージ(statusText)が正しく表示されない問題に対処

--- src/js/majiang/view/paipufile.js
+++ src/js/majiang/view/paipufile.js
@@ -91,6 +91,37 @@ class PaipuStorage {
 
 let _row;
 
+function http_error(e) {
+    const statusText = {
+        '400':  'Bad Request',
+        '401':  'Unauthorized',
+        '402':  'Payment Required',
+        '403':  'Forbidden',
+        '404':  'Not Found',
+        '405':  'Method Not Allowed',
+        '406':  'Not Acceptable',
+        '407':  'Proxy Authentication Required',
+        '408':  'Request Timeout',
+        '409':  'Conflict',
+        '410':  'Gone',
+        '411':  'Length Required',
+        '412':  'Precondition Failed',
+        '413':  'Request Entity Too Large',
+        '414':  'Request-URI Too Long',
+        '415':  'Unsupported Media Type',
+        '416':  'Requested Range Not Satisfiable',
+        '417':  'Expectation Failed',
+        '500':  'Internal Server Error',
+        '501':  'Not Implemented',
+        '502':  'Bad Gateway',
+        '503':  'Service Unavailable',
+        '504':  'Gateway Timeout',
+        '505':  'HTTP Version Not Supported',
+    };
+    return e.statusText == 'error' ? `${e.status} ${statusText[e.status]}`
+                                   : `${e.status} ${e.statusText}`;
+}
+
 module.exports = class PaipuFile {
 
 constructor(node, storage, url, hash) {
@@ -181,7 +212,7 @@ load_paipu(url, hash) {
     }
     const error = e => {
         this.redraw();
-        this.error(`${decodeURI(url)}: ${e.status} ${e.statusText}`);
+        this.error(`${decodeURI(url)}: ${http_error(e)}`);
     }
 
     if (this._paipu.length()) delete this._url;
