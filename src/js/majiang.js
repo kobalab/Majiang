1cfbcfda62bc1dcb86df91ed9f64bb7da47dafb1
Satoshi Kobayashi <koba@yk.rim.or.jp>
2022-03-01 20:46:58 +0800

    2022-03-01 v1.6.6
     - 3シャンテン以前の場合、検討モードに牌の危険度レベルが表示されないバグを修正

--- src/js/majiang.js
+++ src/js/majiang.js
@@ -1,6 +1,6 @@
 /*!
  *
- *  Majiang v1.6.5
+ *  Majiang v1.6.6
  *
  *  Copyright(C) 2017 Satoshi Kobayashi
  *  Released under the MIT license
@@ -10,7 +10,7 @@
 "use strict";
 
 module.exports = {
-    VERSION: '1.6.5',
+    VERSION: '1.6.6',
     Shoupai: require('./majiang/shoupai'),
     Shan:    require('./majiang/shan'),
     He:      require('./majiang/he'),
