a7ce6195d7380f0537fc0283fa467056ea4968fe
Satoshi Kobayashi <koba@yk.rim.or.jp>
2022-12-02 21:32:11 +0800

    v2.0.1 / 2022-12-02
      - #106 何切る解答機で副露直後の牌姿を指定すると例外が発生するバグを修正
      - @kobalab/majiang-ai 1.0.0 → 1.0.1
      - @kobalab/majiang-ui 1.0.0 → 1.0.1
        - 検討モードで全ての牌が「現物」と表示されるバグを修正
      - 脆弱性警告に対処
        - stylus 0.55.0 → 0.59.0

--- src/js/majiang.js
+++ src/js/majiang.js
@@ -1,5 +1,5 @@
 /*!
- *  電脳麻将 v2.0.0
+ *  電脳麻将 v2.0.1
  *
  *  Copyright(C) 2017 Satoshi Kobayashi
  *  Released under the MIT license
@@ -9,6 +9,6 @@
 global.Majiang = require('@kobalab/majiang-core');
 global.Majiang.AI = require('@kobalab/majiang-ai');
 global.Majiang.UI = require('@kobalab/majiang-ui');
-global.Majiang.VERSION = '2.0.0';
+global.Majiang.VERSION = '2.0.1';
 global.jQuery  = require('jquery');
 global.$ = jQuery;
