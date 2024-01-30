57ef5629c91a65d2f93e4f545da180ed41030ffc
Satoshi Kobayashi <koba@yk.rim.or.jp>
2024-01-30 23:48:23 +0800

    #107 ネット対戦用のコードを初期登録

---
+++ src/js/netplay.js
@@ -0,0 +1,138 @@
+/*!
+ *  電脳麻将: ネット対戦 v2.0.1
+ *
+ *  Copyright(C) 2017 Satoshi Kobayashi
+ *  Released under the MIT license
+ *  https://github.com/kobalab/Majiang/blob/master/LICENSE
+ */
+"use strict";
+
+const { hide, show, fadeIn, scale,
+        setSelector, clearSelector  } = Majiang.UI.Util;
+
+const base = location.pathname.replace(/\/[^\/]*?$/,'');
+
+let loaded;
+
+$(function(){
+
+    const pai   = Majiang.UI.pai($('#loaddata'));
+    const audio = Majiang.UI.audio($('#loaddata'));
+
+    const file = new Majiang.UI.PaipuFile($('#file'), 'Majiang.netplay');
+
+    const player = new Majiang.UI.Player($('#board'), pai, audio);
+    player.view  = new Majiang.UI.Board($('#board .board'), pai, audio,
+                                        player.model);
+
+    let sock, myid;
+
+    function init() {
+
+        sock = io('/', { path: `${base}/server/socket.io/`});
+        sock.onAny(console.log);                                    // for DEBUG
+
+        sock.on('HELLO', (user)=>{
+            if (! user) {
+                $('body').attr('class','title');
+                show($('#title .login'));
+            }
+            else {
+                myid = user.id;
+                sock.off('disconnect').on('disconnect', ()=>{
+                    hide($('#file .netplay form'));
+                });
+                sock.off('ERROR').on('ERROR', file.error);
+                sock.off('ROOM').on('ROOM', room);
+
+                show($('#file .netplay form'));
+                fadeIn($('body').attr('class','file'));
+                if (user.icon)
+                    $('#file .netplay img').attr('src', user.icon)
+                                           .attr('title', user.id);
+                $('#file .netplay .name').text(user.name);
+                file.redraw();
+            }
+        });
+
+        sock.on('END', end);
+
+        hide($('#title .loading'));
+    }
+
+    let row, src;
+
+    function room(msg) {
+        if (! row) {
+            row = $('#room .user').eq(0);
+            src = $('img', row).attr('src');
+        }
+        $('body').attr('class','room');
+        $('#room input[name="room"]').val(msg.no);
+        $('#room .room').empty();
+        for (let user of msg.user) {
+            let r = row.clone();
+            if (user.icon) $('img', r).attr('src', user.icon)
+                                      .attr('title', user.id);
+            else           $('img', r).attr('src', src);
+            $('.name', r).text(user.name);
+            if (user.offline) r.addClass('offline');
+            else              r.removeClass('offline');
+            $('#room .room').append(r);
+        }
+        if (msg.user[0].id == myid) show($('#room input[type="submit"]'));
+        else                        hide($('#room input[type="submit"]'));
+        sock.off('START').on('START', start);
+    }
+
+    function start() {
+        $('body').attr('class','board');
+        scale($('#board'), $('#space'))
+        sock.off('ROOM');
+        sock.off('GAME').on('GAME', (msg)=>{
+            if (msg.say) {
+                player._view.say(msg.say.name, msg.say.l);
+            }
+            else if (msg.seq) {
+                player.action(msg, (reply = {})=>{
+                    reply.seq = msg.seq;
+                    sock.emit('GAME', reply);
+                });
+            }
+            else {
+                player.action(msg);
+                if (msg.kaiju && msg.kaiju.log) {
+                    let log = msg.kaiju.log.pop();
+                    for (let data of log) {
+                        player.action(data);
+                    }
+                }
+            }
+        });
+    }
+
+    function end(paipu) {
+        if (paipu) file.add(paipu, 10);
+        fadeIn($('body').attr('class','file'));
+        file.redraw();
+        sock.off('ROOM').on('ROOM', room);
+        $('#file input[name="room"]').val('');
+    }
+
+    $('#file .netplay form').on('submit', (ev)=>{
+        let room = $('input[name="room"]', $(ev.target)).val();
+        sock.emit('ROOM', room);
+        return false;
+    });
+    $('#room form').on('submit', (ev)=>{
+        let room = $('input[name="room"]', $(ev.target)).val();
+        sock.emit('ROOM', room);
+        return false;
+    });
+
+    $(window).on('resize', ()=>scale($('#board'), $('#space')));
+
+    $(window).on('load', ()=>setTimeout(init, 500));
+    if (loaded) $(window).trigger('load');
+});
+$(window).on('load', ()=> loaded = true);
