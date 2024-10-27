b941a211bba9850ca7e02a68aad27738bf95fb92
Satoshi Kobayashi <koba@yk.rim.or.jp>
2024-10-27 17:44:24 +0800

    牌譜エディタ機能を追加

--- src/js/paipu.js
+++ src/js/paipu.js
@@ -16,6 +16,11 @@ $(function(){
     const pai   = Majiang.UI.pai($('#loaddata'));
     const audio = Majiang.UI.audio($('#loaddata'));
 
+    const rule  = Majiang.rule(
+                    JSON.parse(localStorage.getItem('Majiang.rule')||'{}'));
+
+    let file;
+
     const analyzer = (kaiju)=>{
         $('body').addClass('analyzer');
         return new Majiang.UI.Analyzer($('#board > .analyzer'), kaiju, pai,
@@ -35,20 +40,38 @@ $(function(){
         return new Majiang.UI.PaipuStat($('#stat'), paipu_list,
                         ()=>fadeIn($('body').attr('class','file')));
     };
+    const preview = (paipu)=>{
+        $('#board .controller').addClass('paipu')
+        $('body').attr('class','board');
+        scale($('#board'), $('#space'));
+        return new Majiang.UI.Paipu(
+                        $('#board'), paipu, pai, audio, 'Majiang.pref',
+                        ()=>fadeIn($('body').attr('class','editor')),
+                        analyzer);
+    };
+    const editor = (paipu, save)=>{
+        new Majiang.UI.PaipuEditor($('#editor'), paipu, rule, pai,
+                        ()=>{ file.storage(true);
+                              fadeIn($('body').attr('class','file')) },
+                        save, preview);
+        fadeIn($('body').attr('class','editor'));
+    };
 
     if (location.search) {
-        new Majiang.UI.PaipuFile($('#file'), 'Majiang.paipu',
-                                viewer, stat,
+        file = new Majiang.UI.PaipuFile(
+                                $('#file'), 'Majiang.paipu',
+                                viewer, stat, editor,
                                 tenhou_log,
                                 location.search.replace(/^\?/,''),
-                                location.hash.replace(/^#/,'')).redraw();
+                                location.hash.replace(/^#/,''));
     }
     else {
-        new Majiang.UI.PaipuFile($('#file'), 'Majiang.paipu',
-                                viewer, stat,
-                                tenhou_log
-                            ).redraw();
+        file = new Majiang.UI.PaipuFile(
+                                $('#file'), 'Majiang.paipu',
+                                viewer, stat, editor,
+                                tenhou_log);
     }
+    file.redraw();
 
     $(window).on('resize', ()=>scale($('#board'), $('#space')));
 });
